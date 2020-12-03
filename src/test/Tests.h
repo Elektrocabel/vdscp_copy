b22ed8894fbdf27c67289cd8f44b87e07d2a2b07
Elektrocabel <k_en_ny@mail.ru>
2020-12-03 19:05:21 +0100

    copy of vdscp to play around

---
+++ src/test/Tests.h
@@ -0,0 +1,17 @@
+//
+// Created by tobias on 21.11.16.
+//
+
+#ifndef VDS_PROJECT_GTESTS_H
+#define VDS_PROJECT_GTESTS_H
+
+#include "gtest/gtest.h"
+#include "../Manager.h"
+
+ClassProject::Manager manager;
+
+TEST(ConstantsTest, FalseTest) {
+    EXPECT_EQ(manager.False(), 0);
+}
+
+#endif //VDS_PROJECT_TESTS_H
